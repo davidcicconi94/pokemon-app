b30eec6246551525af795d15abee93fe8492f0d1
David <david.cicconi94@gmail.com>
2023-04-23 03:35:41 -0300

    getStaticProps theory

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Text, useTheme } from "@nextui-org/react";
+import { Image, Spacer, Text, useTheme } from "@nextui-org/react";
 import React from "react";
 
 const Navbar = () => {
@@ -16,12 +16,21 @@ const Navbar = () => {
         backgroundColor: theme?.colors.gray200.value,
       }}
     >
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png"
+        alt="imagen.jpg"
+        height={60}
+        width={60}
+      />
       <Text color="red" h2>
         P
       </Text>
       <Text color="white" h3>
         okemón
       </Text>
+
+      <Spacer css={{ flex: 1 }} />
+      <Text>Favoritos</Text>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 
 import { Layout } from "../../components/layouts";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <>
       <Layout title="Pokemon App">
-        <h2>Hola pokemones</h2>
+        <ul>
+          <li>Pokemones</li>
+          <li>Pokemones</li>
+          <li>Pokemones</li>
+          <li>Pokemones</li>
+        </ul>
       </Layout>
     </>
   );
+};
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log("Hola mundo"); // este console.log no aparece en el cliente, sino en la consola del servidor, ya que se ejecuta ahi mismo
+
+  return {
+    props: {}, // estas props son las unicas que llegan al cliente
+  };
 };
 
 export default Home;
